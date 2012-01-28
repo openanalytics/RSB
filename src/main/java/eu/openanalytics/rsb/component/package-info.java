a3e51b8864cba056876bcd2e9144b17232e24885
David Dossot <david@dossot.net>
2012-01-28 15:59:49 -0800

    #147 fixed API specs links

--- src/main/java/eu/openanalytics/rsb/component/package-info.java
+++ src/main/java/eu/openanalytics/rsb/component/package-info.java
@@ -2,9 +2,9 @@
  * <p>This is the R Service Bus (RSB) API.</p>
  *  
  * <h3>SOAP+MTOM API (Synchronous)</h3>
- * <p>The RSB SOAP+MTOM API is fully described by its <a href="/api-specs.html">WSDL and the related XSDs</a>.</p>
+ * <p>The RSB SOAP+MTOM API is fully described by its <a href="../api-specs.html">WSDL and the related XSDs</a>.</p>
  * <h3>REST API (Asynchronous)</h3>
- * <p>The RSB REST API is documented below. Its micro-format is defined in the <a href="/api-specs.html">XSDs for XML message defintions</a>.
+ * <p>The RSB REST API is documented below. Its micro-format is defined in the <a href="../api-specs.html">XSDs for XML message defintions</a>.
  *    The JSON equivalents of these XML messages is built following the Jettison conventions as highlighted in <a href="https://cwiki.apache.org/CXF20DOC/json-support.html">this CXF documentation</a>.</p>
  * <p>The REST API supports a set of HTTP headers:
  * <ul>
