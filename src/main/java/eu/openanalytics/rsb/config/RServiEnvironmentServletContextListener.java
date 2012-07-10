95624075490d87262dde9071fc5f62631df9641c
David Dossot <david@dossot.net>
2012-07-09 17:38:43 -0700

    migrated to rj 1.1.0-SNAPSHOT

--- src/main/java/eu/openanalytics/rsb/config/RServiEnvironmentServletContextListener.java
+++ src/main/java/eu/openanalytics/rsb/config/RServiEnvironmentServletContextListener.java
@@ -1,3 +1,4 @@
+
 package eu.openanalytics.rsb.config;
 
 import java.util.Map;
@@ -16,6 +17,7 @@ import de.walware.ecommons.ECommons.IAppEnvironment;
 import de.walware.ecommons.IDisposable;
 import de.walware.rj.server.RjsComConfig;
 import de.walware.rj.server.client.RClientGraphic;
+import de.walware.rj.server.client.RClientGraphic.InitConfig;
 import de.walware.rj.server.client.RClientGraphicActions;
 import de.walware.rj.server.client.RClientGraphicDummy;
 import de.walware.rj.server.client.RClientGraphicFactory;
@@ -25,63 +27,83 @@ import de.walware.rj.server.client.RClientGraphicFactory;
  * 
  * @author "OpenAnalytics &lt;rsb.development@openanalytics.eu&gt;"
  */
-public class RServiEnvironmentServletContextListener implements ServletContextListener, IAppEnvironment {
+public class RServiEnvironmentServletContextListener implements ServletContextListener, IAppEnvironment
+{
 
     private final Set<IDisposable> stopListeners = new CopyOnWriteArraySet<IDisposable>();
 
     private Log logger;
 
-    public void contextInitialized(final ServletContextEvent sce) {
+    public void contextInitialized(final ServletContextEvent sce)
+    {
         ECommons.init("de.walware.rj.services.eruntime", this);
         logger = LogFactory.getLog("de.walware.rj.servi.pool");
 
-        RjsComConfig.setProperty("rj.servi.graphicFactory", new RClientGraphicFactory() {
-            public Map<String, ? extends Object> getInitServerProperties() {
+        RjsComConfig.setProperty("rj.servi.graphicFactory", new RClientGraphicFactory()
+        {
+            public Map<String, ? extends Object> getInitServerProperties()
+            {
                 return null;
             }
 
-            public RClientGraphic newGraphic(final int devId, final double w, final double h, final boolean active,
-                    final RClientGraphicActions actions, final int options) {
+            public RClientGraphic newGraphic(final int devId,
+                                             final double w,
+                                             final double h,
+                                             final InitConfig config,
+                                             final boolean active,
+                                             final RClientGraphicActions actions,
+                                             final int options)
+            {
                 return new RClientGraphicDummy(devId, w, h);
             }
 
-            public void closeGraphic(final RClientGraphic graphic) {
+            public void closeGraphic(final RClientGraphic graphic)
+            {
                 // NOOP
             }
         });
     }
 
-    public void contextDestroyed(final ServletContextEvent sce) {
-        try {
-            for (final IDisposable listener : this.stopListeners) {
+    public void contextDestroyed(final ServletContextEvent sce)
+    {
+        try
+        {
+            for (final IDisposable listener : this.stopListeners)
+            {
                 listener.dispose();
             }
-        } finally {
+        }
+        finally
+        {
             stopListeners.clear();
         }
     }
 
-    public void addStoppingListener(final IDisposable listener) {
+    public void addStoppingListener(final IDisposable listener)
+    {
         stopListeners.add(listener);
     }
 
-    public void removeStoppingListener(final IDisposable listener) {
+    public void removeStoppingListener(final IDisposable listener)
+    {
         stopListeners.remove(listener);
     }
 
-    public void log(final IStatus status) {
-        switch (status.getSeverity()) {
-        case IStatus.INFO:
-            logger.info(status.getMessage(), status.getException());
-            break;
-        case IStatus.WARNING:
-            logger.warn(status.getMessage(), status.getException());
-            break;
-        case IStatus.ERROR:
-            logger.error(status.getMessage(), status.getException());
-            break;
-        default:
-            logger.debug(status.getMessage(), status.getException());
+    public void log(final IStatus status)
+    {
+        switch (status.getSeverity())
+        {
+            case IStatus.INFO :
+                logger.info(status.getMessage(), status.getException());
+                break;
+            case IStatus.WARNING :
+                logger.warn(status.getMessage(), status.getException());
+                break;
+            case IStatus.ERROR :
+                logger.error(status.getMessage(), status.getException());
+                break;
+            default :
+                logger.debug(status.getMessage(), status.getException());
         }
     }
 }
