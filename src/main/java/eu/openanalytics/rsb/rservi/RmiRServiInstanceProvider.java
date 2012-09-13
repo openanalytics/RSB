3406f515143684a894f899fe144aec0baf762e9e
David Dossot <david@dossot.net>
2012-09-13 08:54:55 -0700

    prefixed name of pooled rservi clients

--- src/main/java/eu/openanalytics/rsb/rservi/RmiRServiInstanceProvider.java
+++ src/main/java/eu/openanalytics/rsb/rservi/RmiRServiInstanceProvider.java
@@ -18,6 +18,7 @@
  *   You should have received a copy of the GNU Affero General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package eu.openanalytics.rsb.rservi;
 
 import java.io.InputStream;
@@ -54,112 +55,154 @@ import eu.openanalytics.rsb.config.Configuration;
  * 
  * @author "OpenAnalytics &lt;rsb.development@openanalytics.eu&gt;"
  */
-public class RmiRServiInstanceProvider implements RServiInstanceProvider {
-    private static class RServiPoolKey {
+public class RmiRServiInstanceProvider implements RServiInstanceProvider
+{
+    private static class RServiPoolKey
+    {
         private final String address, clientId;
 
-        RServiPoolKey(final String address, final String clientId) {
+        RServiPoolKey(final String address, final String clientId)
+        {
             this.address = address;
             this.clientId = clientId;
         }
 
-        String getAddress() {
+        String getAddress()
+        {
             return address;
         }
 
-        String getClientId() {
+        String getClientId()
+        {
             return clientId;
         }
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             return ToStringBuilder.reflectionToString(this);
         }
 
         @Override
-        public int hashCode() {
+        public int hashCode()
+        {
             return HashCodeBuilder.reflectionHashCode(this);
         }
 
         @Override
-        public boolean equals(final Object obj) {
+        public boolean equals(final Object obj)
+        {
             return EqualsBuilder.reflectionEquals(this, obj);
         }
     }
 
-    private static class PooledRServiWrapper implements RServi {
+    private static class PooledRServiWrapper implements RServi
+    {
         private final KeyedObjectPool<RServiPoolKey, PooledRServiWrapper> rServiPool;
         private final RServiPoolKey key;
         private final RServi rServi;
 
-        PooledRServiWrapper(final KeyedObjectPool<RServiPoolKey, PooledRServiWrapper> rServiPool, final RServiPoolKey key,
-                final RServi rServi) {
+        PooledRServiWrapper(final KeyedObjectPool<RServiPoolKey, PooledRServiWrapper> rServiPool,
+                            final RServiPoolKey key,
+                            final RServi rServi)
+        {
             this.rServiPool = rServiPool;
             this.key = key;
             this.rServi = rServi;
         }
 
-        public void close() throws CoreException {
-            try {
+        public void close() throws CoreException
+        {
+            try
+            {
                 rServiPool.returnObject(key, this);
-            } catch (final Exception e) {
+            }
+            catch (final Exception e)
+            {
                 LOGGER.error("Failed to return object to pool", e);
             }
         }
 
-        public void destroy() throws CoreException {
+        public void destroy() throws CoreException
+        {
             rServi.close();
         }
 
-        public RPlatform getPlatform() {
+        public RPlatform getPlatform()
+        {
             return rServi.getPlatform();
         }
 
-        public void evalVoid(final String expression, final IProgressMonitor monitor) throws CoreException {
+        public void evalVoid(final String expression, final IProgressMonitor monitor) throws CoreException
+        {
             rServi.evalVoid(expression, monitor);
         }
 
-        public RObject evalData(final String expression, final IProgressMonitor monitor) throws CoreException {
+        public RObject evalData(final String expression, final IProgressMonitor monitor) throws CoreException
+        {
             return rServi.evalData(expression, monitor);
         }
 
-        public RObject evalData(final String expression, final String factoryId, final int options, final int depth,
-                final IProgressMonitor monitor) throws CoreException {
+        public RObject evalData(final String expression,
+                                final String factoryId,
+                                final int options,
+                                final int depth,
+                                final IProgressMonitor monitor) throws CoreException
+        {
             return rServi.evalData(expression, factoryId, options, depth, monitor);
         }
 
-        public RObject evalData(final RReference reference, final IProgressMonitor monitor) throws CoreException {
+        public RObject evalData(final RReference reference, final IProgressMonitor monitor)
+            throws CoreException
+        {
             return rServi.evalData(reference, monitor);
         }
 
-        public RObject evalData(final RReference reference, final String factoryId, final int options, final int depth,
-                final IProgressMonitor monitor) throws CoreException {
+        public RObject evalData(final RReference reference,
+                                final String factoryId,
+                                final int options,
+                                final int depth,
+                                final IProgressMonitor monitor) throws CoreException
+        {
             return rServi.evalData(reference, factoryId, options, depth, monitor);
         }
 
-        public void assignData(final String expression, final RObject data, final IProgressMonitor monitor) throws CoreException {
+        public void assignData(final String expression, final RObject data, final IProgressMonitor monitor)
+            throws CoreException
+        {
             rServi.assignData(expression, data, monitor);
         }
 
-        public void uploadFile(final InputStream in, final long length, final String fileName, final int options,
-                final IProgressMonitor monitor) throws CoreException {
+        public void uploadFile(final InputStream in,
+                               final long length,
+                               final String fileName,
+                               final int options,
+                               final IProgressMonitor monitor) throws CoreException
+        {
             rServi.uploadFile(in, length, fileName, options, monitor);
         }
 
-        public void downloadFile(final OutputStream out, final String fileName, final int options, final IProgressMonitor monitor)
-                throws CoreException {
+        public void downloadFile(final OutputStream out,
+                                 final String fileName,
+                                 final int options,
+                                 final IProgressMonitor monitor) throws CoreException
+        {
             rServi.downloadFile(out, fileName, options, monitor);
         }
 
-        public byte[] downloadFile(final String fileName, final int options, final IProgressMonitor monitor) throws CoreException {
+        public byte[] downloadFile(final String fileName, final int options, final IProgressMonitor monitor)
+            throws CoreException
+        {
             return rServi.downloadFile(fileName, options, monitor);
         }
 
-        public FunctionCall createFunctionCall(final String name) throws CoreException {
+        public FunctionCall createFunctionCall(final String name) throws CoreException
+        {
             return rServi.createFunctionCall(name);
         }
 
-        public RGraphicCreator createRGraphicCreator(final int options) throws CoreException {
+        public RGraphicCreator createRGraphicCreator(final int options) throws CoreException
+        {
             return rServi.createRGraphicCreator(options);
         }
     }
@@ -172,43 +215,59 @@ public class RmiRServiInstanceProvider implements RServiInstanceProvider {
     private KeyedObjectPool<RServiPoolKey, PooledRServiWrapper> rServiPool;
 
     @PostConstruct
-    public void initialize() {
+    public void initialize()
+    {
         final Config config = configuration.getRServiClientPoolConfig();
-        if (config != null) {
+        if (config != null)
+        {
             initializeRServiClientPool(config);
         }
     }
 
-    private void initializeRServiClientPool(final Config config) {
-        final KeyedPoolableObjectFactory<RServiPoolKey, PooledRServiWrapper> factory = new BaseKeyedPoolableObjectFactory<RServiPoolKey, PooledRServiWrapper>() {
+    public void initializeRServiClientPool(final Config config)
+    {
+        final KeyedPoolableObjectFactory<RServiPoolKey, PooledRServiWrapper> factory = new BaseKeyedPoolableObjectFactory<RServiPoolKey, PooledRServiWrapper>()
+        {
             @Override
-            public PooledRServiWrapper makeObject(final RServiPoolKey key) throws Exception {
+            public PooledRServiWrapper makeObject(final RServiPoolKey key) throws Exception
+            {
                 final RServi rServi = RServiUtil.getRServi(key.getAddress(), key.getClientId());
                 return new PooledRServiWrapper(rServiPool, key, rServi);
             }
 
             @Override
-            public void destroyObject(final RServiPoolKey key, final PooledRServiWrapper rservi) throws Exception {
+            public void destroyObject(final RServiPoolKey key, final PooledRServiWrapper rservi)
+                throws Exception
+            {
                 rservi.destroy();
             }
         };
         rServiPool = new GenericKeyedObjectPoolFactory<RServiPoolKey, PooledRServiWrapper>(factory, config).createPool();
-        LOGGER.info("RServi pool instantiated and configured with: " + ToStringBuilder.reflectionToString(config));
+        LOGGER.info("RServi pool instantiated and configured with: "
+                    + ToStringBuilder.reflectionToString(config));
     }
 
     @PreDestroy
-    public void terminate() throws Exception {
-        if (rServiPool != null) {
+    public void terminate() throws Exception
+    {
+        if (rServiPool != null)
+        {
             rServiPool.close();
             LOGGER.info("RServi pool destroyed");
         }
     }
 
-    public RServi getRServiInstance(final String address, final String clientId, final PoolingStrategy poolingStrategy) throws Exception {
-        if ((rServiPool == null) || (poolingStrategy == PoolingStrategy.NEVER)) {
+    public RServi getRServiInstance(final String address,
+                                    final String clientId,
+                                    final PoolingStrategy poolingStrategy) throws Exception
+    {
+        if ((rServiPool == null) || (poolingStrategy == PoolingStrategy.NEVER))
+        {
             return RServiUtil.getRServi(address, clientId);
-        } else {
-            return rServiPool.borrowObject(new RServiPoolKey(address, clientId));
+        }
+        else
+        {
+            return rServiPool.borrowObject(new RServiPoolKey(address, "pooled-" + clientId));
         }
     }
 }
